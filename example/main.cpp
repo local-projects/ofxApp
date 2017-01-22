2e60a51f442674f274f06cdedc8420df05ff0ae9
Oriol Ferrer Mesia <oriol@uri.cat>
2017-01-21 23:13:37 -0500

    testing how much of a pain supporting gl3 will be

--- example/main.cpp
+++ example/main.cpp
@@ -1,6 +1,5 @@
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofxTimeMeasurements.h"
 #include "ofxApp.h"
 
 //========================================================================
@@ -17,6 +16,9 @@ int main( ){
 	winSettings.numSamples = ofxApp::get().getInt("App/numAASamples", 8);
 	winSettings.width = 1024;
 	winSettings.height = 768;
+	//winSettings.glVersionMajor = 3;
+	//winSettings.glVersionMinor = 2;
+
 
 	shared_ptr<ofAppBaseWindow> win = ofCreateWindow(winSettings);	// sets up the opengl context!
 
