760ab0b94df1c73a2e962e6d055207132ad2aff5
Oriol Ferrer Mesia <oriol@uri.cat>
2017-08-03 21:23:43 -0400

    example create GL2 or GL3 context according to macro

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -6,22 +6,32 @@
 int main( ){
 
 	ofxApp::get().loadSettings(); //ofxApp settings loaded early here.
-						//usually they are loaded automatically when you call app.setup();
-						//but in this case we need to read some settings b4 the app launches
-						//and the openGL context is created (AA samples)
+								//usually they are loaded automatically when you call app.setup();
+								//but in this case we need to read some settings b4 the app launches
+								//and the openGL context is created (AA samples)
 
-	//ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
+	ofxApp::get().loadSettings(); //ofxApp settings loaded here!
 
-	ofGLFWWindowSettings winSettings;
-	winSettings.numSamples = ofxApp::get().getInt("App/numAASamples", 8);
-	winSettings.width = 1024;
-	winSettings.height = 768;
-	//winSettings.glVersionMajor = 3;
-	//winSettings.glVersionMinor = 2;
+	ofGLFWWindowSettings settings;
+	#ifdef NANOVG_GL2_IMPLEMENTATION
+	settings.setGLVersion(2, 1);  // Fixed pipeline
+	#endif
 
+	#ifdef NANOVG_GL3_IMPLEMENTATION
+	settings.setGLVersion(3, 2);  // Programmable pipeline >> you need to define GL_VERSION_3 in you pre-processor macros!
+	#endif
 
-	shared_ptr<ofAppBaseWindow> win = ofCreateWindow(winSettings);	// sets up the opengl context!
+	settings.stencilBits = 0; // 8 for nanovg!
+	settings.width = 1027;
+	settings.height = 768;
+	settings.numSamples = ofxApp::get().getInt("App/numAASamples", 8);
 
-	ofRunApp(win, shared_ptr<ofBaseApp>(new ofApp()));
-	ofRunMainLoop();
+	ofCreateWindow(settings);
+
+	//TIME_SAMPLE_ADD_SETUP_HOOKS();
+	TIME_SAMPLE_SET_FRAMERATE(60);	//if you want setup() to get automatically measured,
+	//you need to create the instance once before setup() is called;
+	//just call any TIME_SAMPLE_* method to do so
+
+	ofRunApp(new ofApp());
 }
